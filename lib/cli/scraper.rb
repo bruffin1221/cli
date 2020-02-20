962c8784109094010c5e6be43e013ec54ffb8b40
Bruce Ruffin <bruffin1221@gmail.com>
2020-02-20 06:44:30 +0000

    driving me crazy!

--- lib/cli/scraper.rb
+++ lib/cli/scraper.rb
@@ -2,16 +2,23 @@ class Cli::Scraper
 
   def self.find_hikes
     gorge=Nokogiri::HTML(open("https://www.oregonhikers.org/field_guide/Columbia_River_Gorge_Hikes"))
-     gorge.css("td").text.strip
-    .each do |trail_info|
-      trail_info.css("td")[1].text
-     binding.pry
+    rows = puts gorge.css("tr[2]").text
+    rows.css("tr[1]").each do |row|
+    puts  trail.css.
+    binding.pry
    end
+ end
 
  end
 
+puts gorge.css("a").text
+gorge.css("tr").each do |trail|
+  puts trail.css("td")
+#end
+# gorge.css("td").each do |trail|
+#   trail.css("tr")[1]
+# end
 
-gorge.css("tr").collect do |trail|
-  trail.css("td")[1]
-end
- #gorge.css("table").name
+ #gorge.css("td").text.strip
+#.each do |trail_info|
+#  trail_info.css("td")[1].text
