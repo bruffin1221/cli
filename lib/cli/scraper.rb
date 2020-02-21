15a215620996781a03a547927d81533a26bdc086
Bruce Ruffin <bruffin1221@gmail.com>
2020-02-21 01:18:38 +0000

    better

--- lib/cli/scraper.rb
+++ lib/cli/scraper.rb
@@ -2,13 +2,22 @@ class Cli::Scraper
 
   def self.find_hikes
     gorge=Nokogiri::HTML(open("https://www.oregonhikers.org/field_guide/Columbia_River_Gorge_Hikes"))
-    gorge.css("tr").map do |trail|
-    puts trail.css("td[4]").text.strip
-    puts trail.css("td[3]").text.strip
-    puts trail.css("td[2]").text.strip
-    puts trail.css("td[1]").text.strip
-    binding.pry
+    trails=[]
+    gorge=Nokogiri::HTML(open("https://www.oregonhikers.org/field_guide/Columbia_River_Gorge_Hikes"))
+    gorge.css("tr").each do |trail|
+      trails<<{:name=>trail.css("td[2]").text.strip,
+        :difficulty=> trail.css("td[4]").text.strip,
+        :length=>trail.css("td[3]").text.strip,
+        :elevation=> trail.css("td[5]").text.strip}
   end
+  trails
+    gorge.css("tr").each do |trail|
+      trails<<{:name=>trail.css("td[2]").text.strip,
+        :difficulty=> trail.css("td[4]").text.strip,
+        :length=>trail.css("td[3]").text.strip,
+        :elevation=> trail.css("td[5]").text.strip}
+  end
+  trails
 end
 
 end
